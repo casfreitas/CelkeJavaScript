03c0481f984bea11c5423dbab498dfc10beee12f
Carlos Freitas <casfreitas@yahoo.com.br>
2022-04-10 19:55:41 -0300

    Como utilizar a estrutura condicional switch com JavaScript

--- custom.js
+++ custom.js
@@ -1,64 +1,18 @@
-var a = 10;
-var b = 12;
-var c = 15;
-var e = 10;
+var tipo = 4;
 
-if (a == "10") {
-    document.write("(==) O valor " + a + " é igual a 10! <br>");
-} else {
-    document.write("(==) O valor " + a + " não é igual a 10! <br>");
-}
-
-if (a === "10") {
-    document.write("(===) O valor " + a + " é idêntico a 10! <br>");
-} else {
-    document.write("(===) O valor " + a + " não é idêntico a 10! <br>");
-}
-
-if (a != "10") {
-    document.write("(!=) O valor " + a + " é diferente a 10! <br>");
-} else {
-    document.write("(!=) O valor " + a + " não é diferente a 10! <br>");
-}
-
-if (a !== "10") {
-    document.write("(!==) O valor " + a + " não é idêntico a 10! <br>");
-} else {
-    document.write("(!==) O valor " + a + " é idêntico a 10! <br>");
-}
-
-if (c > b) {
-    document.write("(>) O valor " + c + " é maior que " + b + "!<br>");
-} else {
-    document.write("(>) O valor " + c + " é menor que " + b + "!<br>");
-}
-
-if (a < b) {
-    document.write("(<) O valor " + a + " é menor que " + b + "!<br>");
-} else {
-    document.write("(<) O valor " + a + " é maior que " + b + "!<br>");
-}
-
-if (a >= e) {
-    document.write("(>=) O valor " + a + " é maior ou igual a" + e + "!<br>");
-} else {
-    document.write("(>=) O valor " + a + " não é maior também não é igual a " + e + "!<br>");
-}
-
-if (a <= e) {
-    document.write("(<=) O valor " + a + " é menor ou igual a" + e + "!<br>");
-} else {
-    document.write("(<=) O valor " + a + " não é menor também não é igual a " + e + "!<br>");
-}
-
-if ((a == 11) && (b == 12)) {
-    document.write("(&&) O valor " + a + " é igual a 11 e o valor " + b + " é igual a 12! <br>");
-} else {
-    document.write("(&&) As duas condições não são verdadeiras, o valor " + a + " não é igual a 11 ou o valor " + b + " não é igual a 12! <br>");
-}
-
-if ((a == 11) || (b == 12)) {
-    document.write("(||) O valor " + a + " é igual a 11 ou o valor " + b + " é igual a 12! <br>");
-} else {
-    document.write("(||) Nenhuma das duas condições são verdadeiras, o valor " + a + " não é igual a 11 é o valor " + b + " não é igual a 12! <br>");
+switch (tipo) {
+    case 1:
+        document.write("Apartamento");
+        break;
+    case 2:
+        document.write("Casa");
+        break;
+    case 3:
+        document.write("Sala Comercial");
+        break;
+    case 4:
+        document.write("Sobrado");
+        break;
+    default:
+        document.write("A chave não corresponde a nenhum tipo de imóvel!");
 }
