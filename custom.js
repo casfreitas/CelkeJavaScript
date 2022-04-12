46499aa4e619d70031a2300c73cc964731a8d14e
Carlos Freitas <casfreitas@yahoo.com.br>
2022-04-11 21:29:01 -0300

    Como usar o WHILE e Do WHILE com JavaScript

--- custom.js
+++ custom.js
@@ -1,18 +1,25 @@
-var tipo = 4;
+var inicio = 1;
 
-switch (tipo) {
-    case 1:
-        document.write("Apartamento");
-        break;
-    case 2:
-        document.write("Casa");
-        break;
-    case 3:
-        document.write("Sala Comercial");
-        break;
-    case 4:
-        document.write("Sobrado");
-        break;
-    default:
-        document.write("A chave não corresponde a nenhum tipo de imóvel!");
+while (inicio <= 10) {
+    document.write("Número do cliente: " + inicio + "<br>");
+    inicio++;
 }
+
+var prestacao = 1;
+var qnt_prestacoes = 12;
+
+// DO WHILE Executa ao menos 1 vez
+do {
+    document.write("Número da prestações: " + prestacao + "<br>");
+    prestacao++;
+} while (prestacao <= qnt_prestacoes)
+
+var num_prestacao = 1;
+var num_qnt_prestacoes = 4;
+var total = 120;
+var valor_prestacao = total / num_qnt_prestacoes;
+
+do {
+    document.write("Número da prestação: " + num_prestacao + " valor " + valor_prestacao + "<br>");
+    num_prestacao++;
+} while (num_prestacao <= num_qnt_prestacoes)
